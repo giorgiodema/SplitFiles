04741e2a0c13084b978ab78c2a513bc17f6273fb
Giorgio De Magistris <giorgiodema@gmail.com>
2020-05-11 15:16:48 +0200

    error handling

--- split_file.py
+++ split_file.py
@@ -2,6 +2,7 @@ import os
 import sys
 import math
 from functools import reduce
+from tqdm import tqdm
 
 # To split file     : $ python split_file.py SPLIT PATH_TO_FILE_TO_SPLIT SPLIT_SIZE(MB)
 # To recreate file  : $ python split_file.py RECREATE PATH_TO_FIRST_SPLIT 
@@ -32,7 +33,7 @@ def split(path,size):
 
     if file_size <= split_size:
         print("Error: the size of the split should be less then the size of the original file")
-        return False
+        return -1
     
     splits = int(math.ceil(float(file_size)/float(split_size)))
     file_name_no_ext = file_name \
@@ -42,11 +43,12 @@ def split(path,size):
     writeHeader(split0,file_name,splits)
 
     with open(path,"rb") as file:
-        for s in range(splits):
+        for s in tqdm(range(splits),desc="Creating Splits"):
             split = os.path.join(directory,file_name_no_ext + ".SPLIT_{}".format(s))
             with open(split,"ab") as split:
                 data = file.read(split_size)
                 split.write(data)
+    return splits
 
 def recreate(path):
     dir,_ = os.path.split(path)
@@ -60,17 +62,63 @@ def recreate(path):
 
     file_name = "RECONSTRUCTED_"+file_name
     with open(os.path.join(dir,file_name),"wb") as f:
-        for s in range(splits):
+        for s in tqdm(range(splits),desc="Recreating File"):
             split_path = os.path.join(dir,file_name_no_ext+".SPLIT_{}".format(s))
+            if not os.path.exists(split_path):
+                print("Error: missing split n {}".format(s))
             with open(split_path,"rb") as split:
                 # consume header of the first split
                 if s==0:
                     for i in range(3):
                         split.readline()
                 f.write(split.read())
+    return True
     
 
+if len(sys.argv) < 2 and (sys.argv[1] not in ["SPLIT","RECREATE"]):
+    print("Usage:\n\
+        To split file     : $ python split_file.py SPLIT PATH_TO_FILE_TO_SPLIT SPLIT_SIZE(MB)\n\
+        To recreate file  : $ python split_file.py RECREATE PATH_TO_FIRST_SPLIT ")
+    exit(-1)
 
-#split("C:\\Users\\giorg\\Desktop\\SplitFiles\\Fantastic.Beasts.The.Crimes.Of.Grindelwald.2018.EXTENDED.1080p.BluRay.x265-RARBG.mp4",100)
-recreate("C:\\Users\\giorg\\Desktop\\SplitFiles\\FantasticBeastsTheCrimesOfGrindelwald2018EXTENDED1080pBluRayx265-RARBG.SPLIT_0")
+if sys.argv[1] =="SPLIT":
+    if len(sys.argv)!=4:
+        print("Usage:\n\
+            To split file     : $ python split_file.py SPLIT PATH_TO_FILE_TO_SPLIT SPLIT_SIZE(MB)\n\
+            To recreate file  : $ python split_file.py RECREATE PATH_TO_FIRST_SPLIT ")
+        exit(-1)
 
+    path_to_file = os.path.normpath(sys.argv[2])
+    split_size = None
+    try:
+        split_size = int(sys.argv[3])
+    except ValueError:
+        print("Error: Invalid Size")
+    if not os.path.isfile(path_to_file):
+        print("Error: Invalid Path")
+
+    if split(path_to_file,split_size) > 0:
+        print("Completed.")
+    else:
+        print("Aborted.")
+
+if sys.argv[1] == "RECREATE":
+    if len(sys.argv)!=3:
+        print("Usage:\n\
+            To split file     : $ python split_file.py SPLIT PATH_TO_FILE_TO_SPLIT SPLIT_SIZE(MB)\n\
+            To recreate file  : $ python split_file.py RECREATE PATH_TO_FIRST_SPLIT ")
+        exit(-1)
+
+    path_to_split = os.path.normpath(sys.argv[2])
+    if not os.path.isfile(path_to_split):
+        print("Error: Invalid Path")
+
+    if recreate(path_to_split):
+        print("Completed.")
+    else:
+        print("Aborted.")
+
+
+    
+
+    
